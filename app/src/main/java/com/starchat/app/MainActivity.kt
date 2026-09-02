1e6ade460fdc20715546b6dea38e93d457e75535
yejrjrkekek <midousametime@gmail.com>
2026-09-02 12:06:46 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/starchat/app/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.starchat.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Greeting("StarChat")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Welcome to $name!")
+}
